3801a075e1b3df51017c13bf2e6d0d96b06c5383
Stephen Tu <stephentu@csail.mit.edu>
2012-06-15 14:49:10 -0400

    use macro to guard verboseness

--- execution/common.hh
+++ execution/common.hh
@@ -2,13 +2,18 @@
 
 #include <cstdio>
 
-#define dprintf(args...) \
-  do { \
-    fprintf(stderr, "%s: ", __PRETTY_FUNCTION__); \
-    fprintf(stderr, args); \
-  } while (0)
-
+#define EXECUTION_VERBOSE 1
 #define EXTRA_SANITY_CHECKS 1
+
+#ifdef EXECUTION_VERBOSE
+  #define dprintf(args...) \
+    do { \
+      fprintf(stderr, "%s: ", __PRETTY_FUNCTION__); \
+      fprintf(stderr, args); \
+    } while (0)
+#else
+  #define dprintf(args...)
+#endif /* EXECUTION_VERBOSE */
 
 #ifdef EXTRA_SANITY_CHECKS
   #define SANITY(x) assert(x)
