dd2ee7b99c38e04b6bbd4f96723c690eeb4f23f1
Stephen Tu <stephentu@csail.mit.edu>
2012-06-15 18:26:34 -0400

    forgot missing config file

---
+++ plainbenchmark/db_config.h
@@ -0,0 +1,25 @@
+// copy this file to db_config.h, filling in the appropriate values
+#define DB_HOSTNAME "vise4.csail.mit.edu"
+#define DB_USERNAME "stephentu"
+#define DB_PASSWORD "letmein"
+#define DB_PORT     5432
+
+//#define DB_DATABASE "tpch_0_05"
+//#define LINEITEM_NAME "lineitem_int"
+//#define CUSTOMER_NAME "customer_int"
+//#define ORDERS_NAME "orders_int"
+//#define PART_NAME "part_int"
+//#define PARTSUPP_NAME "partsupp_int"
+//#define SUPPLIER_NAME "supplier_int"
+//#define REGION_NAME "region_int"
+//#define NATION_NAME "nation_int"
+
+#define DB_DATABASE "tpch_10_00"
+#define LINEITEM_NAME "lineitem_int_inflate"
+#define CUSTOMER_NAME "customer_int_inflate"
+#define ORDERS_NAME "orders_int_inflate"
+#define PART_NAME "part_int_inflate"
+#define PARTSUPP_NAME "partsupp_int_inflate"
+#define SUPPLIER_NAME "supplier_int_inflate"
+#define REGION_NAME "region_int_inflate"
+#define NATION_NAME "nation_int_inflate"
