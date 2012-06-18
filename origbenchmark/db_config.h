15b097f30e4fa0fd4ba024e1bf42244f00d9d6ef
Stephen Tu <stephentu@csail.mit.edu>
2012-06-17 20:03:17 -0400

    add missing config file

---
+++ origbenchmark/db_config.h
@@ -0,0 +1,19 @@
+// copy this file to db_config.h, filling in the appropriate values
+#define DB_HOSTNAME "vise4.csail.mit.edu"
+#define DB_USERNAME "stephentu"
+#define DB_PASSWORD "letmein"
+#define DB_PORT     5432
+
+#define DB_DATABASE "tpch_10_00"
+
+#define LINEITEM_ENC_NAME "lineitem_enc_orig_system"
+#define CUSTOMER_ENC_NAME "customer_enc_orig_system"
+#define PARTSUPP_ENC_NAME "partsupp_enc_orig_system"
+#define ORDERS_ENC_NAME   "orders_enc_orig_system"
+
+#define PART_ENC_NAME     "part_enc"
+#define REGION_ENC_NAME   "region_enc"
+#define SUPPLIER_ENC_NAME "supplier_enc"
+#define NATION_ENC_NAME   "nation_enc"
+
+#define CRYPTO_USE_OLD_OPE false
