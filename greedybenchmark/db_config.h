3ba16ad0ac980015f36b35b066dfcc23d3565abd
Stephen Tu <stephentu@csail.mit.edu>
2012-06-20 03:07:09 -0400

    minor config changes to greedybenchmark

--- greedybenchmark/db_config.h
+++ greedybenchmark/db_config.h
@@ -11,15 +11,15 @@
 
 // this is not auto generated
 
-//#define LINEITEM_ENC_NAME "lineitem_enc_cryptdb_greedy"
-//#define CUSTOMER_ENC_NAME "customer_enc_cryptdb_greedy"
-//#define PARTSUPP_ENC_NAME "partsupp_enc_cryptdb_greedy"
-//#define ORDERS_ENC_NAME   "orders_enc_cryptdb_greedy"
+#define LINEITEM_ENC_NAME "lineitem_enc_cryptdb_greedy"
+#define CUSTOMER_ENC_NAME "customer_enc_cryptdb_greedy"
+#define PARTSUPP_ENC_NAME "partsupp_enc_cryptdb_greedy"
+#define ORDERS_ENC_NAME   "orders_enc_cryptdb_greedy"
 
-#define LINEITEM_ENC_NAME "lineitem_enc_rowid"
-#define CUSTOMER_ENC_NAME "customer_enc_rowid"
-#define PARTSUPP_ENC_NAME "partsupp_enc_rowid"
-#define ORDERS_ENC_NAME   "orders_enc"
+//#define LINEITEM_ENC_NAME "lineitem_enc_rowid"
+//#define CUSTOMER_ENC_NAME "customer_enc_rowid"
+//#define PARTSUPP_ENC_NAME "partsupp_enc_rowid"
+//#define ORDERS_ENC_NAME   "orders_enc"
 
 #define PART_ENC_NAME     "part_enc"
 #define REGION_ENC_NAME   "region_enc"
