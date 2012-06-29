3a8121dd09019c3fb78774708ee2a54eb03cb589
Stephen Tu <stephentu@csail.mit.edu>
2012-06-28 23:56:41 -0400

    try on vise5 w/ small data for now

--- hardbenchmark/db_config_opt.h
+++ hardbenchmark/db_config_opt.h
@@ -1,8 +1,9 @@
-#define DB_HOSTNAME "vise4.csail.mit.edu"
+#define DB_HOSTNAME "vise5.csail.mit.edu"
 #define DB_USERNAME "stephentu"
 #define DB_PASSWORD "letmein"
 
-#define DB_DATABASE "tpch_10_00"
+#define DB_DATABASE "tpch_1_00"
+//#define DB_DATABASE "tpch_10_00"
 //#define DB_DATABASE "tpch_0_05"
 
 #define DB_PORT     5432
@@ -12,14 +13,14 @@
 // this is not auto generated
 
 #define LINEITEM_ENC_NAME "lineitem_enc_cryptdb_opt"
-#define CUSTOMER_ENC_NAME "customer_enc_rowid"
+#define CUSTOMER_ENC_NAME "customer_enc_cryptdb_opt"
 #define PARTSUPP_ENC_NAME "partsupp_enc_cryptdb_opt"
 //#define LINEITEM_ENC_NAME "lineitem_enc"
 //#define CUSTOMER_ENC_NAME "customer_enc"
 //#define PARTSUPP_ENC_NAME "partsupp_enc"
 
-#define ORDERS_ENC_NAME   "orders_enc"
+#define ORDERS_ENC_NAME   "orders_enc_cryptdb_opt"
 #define PART_ENC_NAME     "part_enc_cryptdb_opt"
-#define REGION_ENC_NAME   "region_enc"
+#define REGION_ENC_NAME   "region_enc_cryptdb_opt"
 #define SUPPLIER_ENC_NAME "supplier_enc_cryptdb_opt"
-#define NATION_ENC_NAME   "nation_enc"
+#define NATION_ENC_NAME   "nation_enc_cryptdb_opt"
