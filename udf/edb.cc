d421bf1eb4e4aa618c3d70bb5318c4f93d6aa03b
raluca <raluca@ubuntu.(none)>
2012-10-10 10:20:49 -0400

    making server socket static in edb.cc ; do not hard code numeric values for message types for the communication, instead use an enum with human meaningful names -- see util/net.hh

--- udf/edb.cc
+++ udf/edb.cc
@@ -30,8 +30,8 @@ uint64_t make_mask(){
 
 extern "C" {
 
-typedef unsigned long long ulonglong;
-typedef long long longlong;
+    typedef unsigned long long ulonglong;
+    typedef long long longlong;
 
 
 #include <mysql.h>
@@ -39,92 +39,75 @@ typedef long long longlong;
 
 
 //UDF that creates the ops server
-my_bool  create_ops_server_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
-ulonglong create_ops_server(UDF_INIT *initid, UDF_ARGS *args, char *is_null,
-                         char *error);
-void  create_ops_server_deinit(UDF_INIT *initid);
+    my_bool  create_ops_server_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+    ulonglong create_ops_server(UDF_INIT *initid, UDF_ARGS *args, char *is_null,
+				char *error);
+    void  create_ops_server_deinit(UDF_INIT *initid);
 
 
 // UDF that interacts with ops server with respect to some encryption
-my_bool  get_ops_server_init(UDF_INIT *initid, UDF_ARGS *args,
-                               char *message);
-void     get_ops_server_deinit(UDF_INIT *initid);
-char *   get_ops_server(UDF_INIT *initid, UDF_ARGS *args, char *result,
-                          unsigned long *length, char *is_null, char *error);
+    my_bool  get_ops_server_init(UDF_INIT *initid, UDF_ARGS *args,
+				 char *message);
+    void     get_ops_server_deinit(UDF_INIT *initid);
+    char *   get_ops_server(UDF_INIT *initid, UDF_ARGS *args, char *result,
+			    unsigned long *length, char *is_null, char *error);
 
 // UDF to request OPE encryption
-my_bool  ope_enc_init(UDF_INIT *initid, UDF_ARGS *args,
-                               char *message);
-void     ope_enc_deinit(UDF_INIT *initid);
-long long   ope_enc(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error);
+    my_bool  ope_enc_init(UDF_INIT *initid, UDF_ARGS *args,
+			  char *message);
+    void     ope_enc_deinit(UDF_INIT *initid);
+    long long   ope_enc(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *error);
 
 } /* extern C */
 /*
-static uint64_t
-getui(UDF_ARGS * args, int i)
-{
-    return (uint64_t) (*((ulonglong *) args->args[i]));
-}
+  static uint64_t
+  getui(UDF_ARGS * args, int i)
+  {
+  return (uint64_t) (*((ulonglong *) args->args[i]));
+  }
 */
 /*static string
-getstring(UDF_ARGS * args, int i)
-{
-    return string(args->args[i], args->lengths[i]);
-}*/
+  getstring(UDF_ARGS * args, int i)
+  {
+  return string(args->args[i], args->lengths[i]);
+  }*/
 
+static int sock_ser = create_and_connect(OPE_SERVER_HOST, OPE_SEVER_PORT);
+    
 extern "C" {
 
-struct ope_state {
-    int sock_cl;
-    int sock_ser;
-};
-    
-my_bool  ope_enc_init(UDF_INIT *initid, UDF_ARGS *args, char *message){
-    ope_state * os = new ope_state();
-    os->sock_cl = create_and_connect(OPE_CLIENT_HOST, OPE_CLIENT_PORT);
-    os->sock_ser = create_and_connect(OPE_SERVER_HOST, OPE_SERVER_PORT);
-
-    initid->ptr = (char *) os;
-	
-    return 0;
-}
-void ope_enc_deinit(UDF_INIT *initid){
-
-    ope_state *os = (ope_state*)initid->ptr;
-    close(os->sock_cl);
-    close(os->sock_ser);
-    
-    return;
-}
-
-long long ope_enc(UDF_INIT *initid, UDF_ARGS *args,
-    char *is_null, char *error){
-
-    ope_state * os = (ope_state *) initid->ptr;
-    
-    long long det_val;
-
-    det_val = *((long long*) args->args[0]);
-
-    cerr << "inside UDF \n";
-
-    char mode;
-    mode = *((char*) args->args[1]);
-
-    bool imode;
-    if (mode=='i'){
-        imode=true;
-    } else {
-        imode=false;
+    my_bool  ope_enc_init(UDF_INIT *initid, UDF_ARGS *args, char *message){
+	return 0;
     }
-    cerr<<"mode: "<<mode<<" imode: "<<imode<<endl;
+    void ope_enc_deinit(UDF_INIT *initid){
+	return;
+    }
 
-    mask=make_mask();
+    long long ope_enc(UDF_INIT *initid, UDF_ARGS *args,
+		      char *is_null, char *error){
 
-    int counter = 0;
-    while (counter < 2) {
-	counter++;
+	ope_state * os = (ope_state *) initid->ptr;
+    
+	long long det_val;
+
+	det_val = *((long long*) args->args[0]);
+
+	cerr << "inside UDF \n";
+
+	char mode;
+	mode = *((char*) args->args[1]);
+
+	bool imode;
+	if (mode=='i'){ // we need to insert value
+	    imode=true;
+
+	} else { // we do not insert value, just for query
+	    imode=false;
+
+	}
 	
+	cerr << "mode: " << mode << " imode: " << imode << endl;
+
         char buffer[1024];
     
         memset(buffer, '\0', 1024);
@@ -134,153 +117,98 @@ long long ope_enc(UDF_INIT *initid, UDF_ARGS *args,
         string msg = o.str();
         send(os->sock_ser, msg.c_str(), msg.size(), 0);
         recv(os->sock_ser, buffer, 1024, 0);
-        uint64_t early_v, early_pathlen, early_index;
-        uint64_t v = 0;
-        uint64_t nbits = 0;
 
-        istringstream iss(buffer);
-        iss >> early_v;
-        iss >> early_pathlen;
-	iss >> early_index;
-
-        if(early_v!=(uint64_t)-1 && early_pathlen!=(uint64_t)-1 && early_index!=(uint64_t)-1){
-            cerr<<"In early return"<<endl;
-            v = early_v;
-            nbits = early_pathlen+num_bits;
-            v = (v<<num_bits) | early_index;  
-            uint64_t rtn_val = (v<<(64-nbits)) | (mask<<(64-num_bits-nbits));
-            cerr<<"Returning "<<rtn_val<<endl;
-            return (v<<(64-nbits)) | (mask<<(64-num_bits-nbits));
-        }else{
-            cerr<<"No early return, must insert"<<endl;
-            
-	    memset(buffer, '\0', 1024);
-
-            ostringstream o_client;
-            if (imode) {
-		o_client<<"14 "<<det_val;
-	    }
-            else {
-		o_client<<"15 "<<det_val;
-	    }
-	    
-            string client_msg = o_client.str();
-            send(os->sock_cl, client_msg.c_str(), client_msg.size(), 0);
-	    cerr << "before waiting for receive\n";
-	    recv(os->sock_cl, buffer, 1024, 0);
-	    cerr << "after waiting for receive\n";
-            istringstream iss(buffer);
-            cerr << "Buffer for " << det_val << " is " << buffer << endl;
-            string output;
-            iss >> output;
-            cerr<<"Output = "<<output<<endl;
-            if (output=="DONE") {
-		
-                if(!imode) cerr<<"Output done but imode = false"<<endl;
-                cerr<<"Received done!"<<endl;
-
-	    } else if(output=="15"){
-
-		if(imode) cerr<<"Output 15 but imode = true"<<endl;
-                cerr<<"In 15 waa??"<<endl;
-                uint64_t ope_rtn;
-                iss>>ope_rtn;
-                cout<<"Return ope_rtn="<<ope_rtn<<endl;
+	istringstream iss(buffer);
+	cerr << "Buffer for " << det_val << " is " << buffer << endl;
+	
+	uint64_t ope_rtn;
+	iss >> ope_rtn;
+	cerr << "Return ope_rtn="<<ope_rtn<<endl;
               
-                return ope_rtn;
-		
-            } else {
-                cerr<<"Received weird string: "<<buffer<<endl;
-            }
-          
-        }        
+	return ope_rtn;
+	
     }
 
-    assert_s(false, "should not have returned from the while\n");
-    return -1;
+    my_bool
+    create_ops_server_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
+    {
+	return 0;	
+    }
 
-}
+    ulonglong
+    create_ops_server(UDF_INIT *initid, UDF_ARGS *args, char *is_null,
+		      char *error) {
 
-my_bool
-create_ops_server_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
-{
-    return 0;	
-}
+	cerr<<"Calling create_ops_server function\n";
 
-ulonglong
-create_ops_server(UDF_INIT *initid, UDF_ARGS *args, char *is_null,
-			    char *error) {
-
-    cerr<<"Calling create_ops_server function\n";
-
-    string command = "/usr/lib/mysql/plugin/server";
+	string command = "/usr/lib/mysql/plugin/server";
     
-    pid_t pid = fork();
+	pid_t pid = fork();
 
-    if (pid == 0) {
-	// child
-    cerr<<"In child now \n";
-	// Frank, below replace exampleserver with the name of the
-	// ope server. 
-	errno = 0;
-	execl(command.c_str(),
-	      "server",
+	if (pid == 0) {
+	    // child
+	    cerr<<"In child now \n";
+	    // Frank, below replace exampleserver with the name of the
+	    // ope server. 
+	    errno = 0;
+	    execl(command.c_str(),
+		  "server",
 /*	      arg0.c_str(), 
 	      arg1.c_str(),
 	      arg2.c_str(),*/
-	      (char *) 0);
-	//error, execl should not return
-	cerr << "child failed to execute " << strerror(errno) << errno << " \n";
-	exit(-1);
+		  (char *) 0);
+	    //error, execl should not return
+	    cerr << "child failed to execute " << strerror(errno) << errno << " \n";
+	    exit(-1);
+	}
+
+	//back in parent
+    
+	if (pid < 0) {
+	    //failed to fork
+	    cerr << "failed to fork ops server \n";
+	    return -1;
+	} else {
+	    cerr << "forked ops server; pid: " << pid << " \n";
+	    return 0;
+	}
     }
 
-    //back in parent
+    void
+    create_ops_server_deinit(UDF_INIT *initid) {
+    }
+
     
-    if (pid < 0) {
-	//failed to fork
-	cerr << "failed to fork ops server \n";
-	return -1;
-    } else {
-	cerr << "forked ops server; pid: " << pid << " \n";
+    my_bool
+    get_ops_server_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
+    {
 	return 0;
     }
-}
 
-void
-create_ops_server_deinit(UDF_INIT *initid) {
-}
+    void
+    get_ops_server_deinit(UDF_INIT *initid)
+    {
+	/*
+	 * in mysql-server/sql/item_func.cc, udf_handler::fix_fields
+	 * initializes initid.ptr=0 for us.
+	 */
+	if (initid->ptr)
+	    delete[] initid->ptr;
+    }
 
-    
-my_bool
-get_ops_server_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
-{
-    return 0;
-}
-
-void
-get_ops_server_deinit(UDF_INIT *initid)
-{
-    /*
-     * in mysql-server/sql/item_func.cc, udf_handler::fix_fields
-     * initializes initid.ptr=0 for us.
-     */
-    if (initid->ptr)
-        delete[] initid->ptr;
-}
-
-char *
-get_ops_server(UDF_INIT *initid, UDF_ARGS *args,
-                 char *result, unsigned long *length,
-                 char *is_null, char *error) {
+    char *
+    get_ops_server(UDF_INIT *initid, UDF_ARGS *args,
+		   char *result, unsigned long *length,
+		   char *is_null, char *error) {
    
-    string addr = "127.0.0.1:1111";
-    char * res = (char*) addr.c_str();
-    initid->ptr = res;
-    memcpy(res, "127.0.0.1:1111", strlen(res));
-    *length = strlen(res);
+	string addr = "127.0.0.1:1111";
+	char * res = (char*) addr.c_str();
+	initid->ptr = res;
+	memcpy(res, "127.0.0.1:1111", strlen(res));
+	*length = strlen(res);
   
-    return (char*) initid->ptr;
-}
+	return (char*) initid->ptr;
+    }
 
     
 } /* extern "C" */
--- util/net.hh
+++ util/net.hh
@@ -2,18 +2,18 @@
 
 #include <string>
 
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <resolv.h>
+
 const int OPE_SERVER_PORT = 1111;
 const int OPE_CLIENT_PORT = 1112;
 
 const std::string OPE_SERVER_HOST = "127.0.0.1"; // we will need to get these in
 						 // as inputs instead
 const std::string OPE_CLIENT_HOST = "127.0.0.1";
-
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <resolv.h>
 
 
 // creates a socket and binds it on port
@@ -24,3 +24,6 @@ create_and_bind(int host_port);
 // creates a socket and connects through it
 int
 create_and_connect(std::string host_name, int host_port);
+
+// network message types
+typedef enum MsgType {UDF_ENC, START_INTERACT};
