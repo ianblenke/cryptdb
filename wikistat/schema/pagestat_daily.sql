482492fc294f30561f027e89f46e5208c2214fb9
Stephen Tu <stephentu@csail.mit.edu>
2012-11-01 00:57:58 -0400

    add missing table

---
+++ wikistat/schema/pagestat_daily.sql
@@ -0,0 +1,6 @@
+CREATE TABLE pagestat_daily (
+   page_id INTEGER NOT NULL,
+   date_id_from INTEGER NOT NULL,
+   date_id_to INTEGER NOT NULL,
+   project_id INTEGER NOT NULL,
+   page_count INTEGER NOT NULL );
